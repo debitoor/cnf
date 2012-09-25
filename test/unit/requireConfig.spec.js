f1b641d133b937a32cd0e34c9849ac2032c642a6
mafintosh <mathiasbuus@gmail.com>
2012-09-25 10:12:59 +0200

    better tests

--- test/unit/requireConfig.spec.js
+++ test/unit/requireConfig.spec.js
@@ -13,45 +13,57 @@ describe('config', function() {
 
 	it('should load development', function() {
 		var config = require('../../source/config');
+
 		expect(config.me).to.eql('development');
-		expect(config.nested.val).to.eql(42);
+		expect(config.nested.val).to.equal(42);
 	});
 	it('should load development explicit', function() {
 		process.env.APP_ENV = 'development';
 		var config = require('../../source/config');
+
 		expect(config.me).to.eql('development');
-		expect(config.nested.val).to.eql(42);
+		expect(config.nested.val).to.equal(42);
 	});
 	it('shoud load production', function() {
 		process.env.APP_ENV = 'production';
 		var config = require('../../source/config');
+
 		expect(config.me).to.eql('production');
-		expect(config.nested.val).to.eql(84);
+		expect(config.nested.val).to.equal(84);
 	});
 	it('should fail to load', function() {
 		process.env.APP_ENV = 'meh';
-		var config;
-		try {
-			config = require('../../source/config');
-		} catch (err) {
-		}
-		expect(config).to.not.be.ok;
+		var requireConfig = function() {
+			require('config');
+		};
+
+		expect(requireConfig).to['throw'](Error);
 	});
 	it('should override with cli args', function() {
 		process.argv = ['node', 'file.js', '--app.me','hello'];
 		var config = require('../../source/config');
-		expect(config.me).to.eql('hello');
-		expect(config.nested.val).to.eql(42);
+
+		expect(config.me).to.equal('hello');
+		expect(config.nested.val).to.equal(42);
 	});
 	it('should not override non prefixed cli args', function() {
 		process.argv = ['node', 'file.js', '--me','hello'];
 		var config = require('../../source/config');
-		expect(config.me).to.eql('development');
-		expect(config.nested.val).to.eql(42);
+
+		expect(config.me).to.equal('development');
+		expect(config.nested.val).to.equal(42);
 	});
-	it('should guess config arg type', function() {
+	it('should parse numbers', function() {
 		process.argv = ['node', 'file.js', '--app.me','424'];
 		var config = require('../../source/config');
-		expect(config.me).to.eql(424);
+
+		expect(config.me).to.equal(424);
+	});
+	it('should parse booleans', function() {
+		process.argv = ['node', 'file.js', '--app.me','--app.nested.val','false'];
+		var config = require('../../source/config');
+
+		expect(config.me).to.equal(true);
+		expect(config.nested.val).to.equal(false);
 	});
 });
